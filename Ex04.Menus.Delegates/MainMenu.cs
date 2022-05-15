1cddaec6147984954abcb4714ff6a1d56693252b
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-05-15 13:32:25 +0300

    first commit

---
+++ Ex04.Menus.Delegates/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    public class MainMenu
+    {
+        private List<MenuItem> m_MenuItems;
+
+        public void Show()
+        {
+            bool stopLoop = false;
+            MenuItem currMenuItem = null;
+
+            while (stopLoop == false)
+            {
+                if (currMenuItem == null)
+                {
+                    Console.WriteLine("***** Main Menu *****");
+                }
+                else
+                {
+                    Console.WriteLine("***** {0} *****", currMenuItem.Text);
+                }
+
+
+
+            }
+        }
+
+    }
+}
---
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    //public delegate Action<T>
+    class MenuItem
+    {
+        private List<MenuItem> m_SubMenuItems = null;
+        private string m_Text = string.Empty;
+
+        public event Action<MenuItem> ItemClicked;
+
+        public MenuItem()
+        {
+
+        }
+
+        public string Text
+        {
+            get
+            {
+                return m_Text;
+            }
+
+            set
+            {
+                m_Text = value;
+            }
+        }
+
+    }
+}
